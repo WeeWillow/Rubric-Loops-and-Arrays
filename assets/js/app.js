56f36668277bd410c16124bdd2217fd7fdc47191
WeeWillow <weecalamy@gmail.com>
2024-02-14 21:58:32 +0100

    start

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// random cat minion generator/creator
+// hosted by Kevin the cat
+
+// arrays
+const name = ["Bertil", "Gerda", "Olga", "Agnes", "Sigurd", "Holger", "Bente", "Inger", "Jens"];
+const title = ["title", "title", "title", "title", "title", "title"];
+const role = ["role", "role", "role", "role", "role", "role", "role"];
+const image = ["image", "image", "image", "image", "image", "image"];
