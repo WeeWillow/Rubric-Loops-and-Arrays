1608411022ceb9a6804917b9fd15267606b297a6
WeeWillow <weecalamy@gmail.com>
2024-02-15 13:36:18 +0100

    iuh... update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,18 @@
 // hosted by Kevin the cat
 
 // arrays
-const name = ["Bertil", "Gerda", "Olga", "Agnes", "Sigurd", "Holger", "Bente", "Inger", "Jens"];
+// array of names, for random generation
+const nameof = ["Bertil", "Gerda", "Olga", "Agnes", "Sigurd", "Holger", "Bente", "Inger", "Jens"];
+console.log('nameof:', nameof)
+// array of titles, for random generation
 const title = ["title", "title", "title", "title", "title", "title"];
+// array of roles, for random generation
 const role = ["role", "role", "role", "role", "role", "role", "role"];
+// array of traits, for random generation
+const trait = ["trait", "trait", "trait", "trait", "trait"];
+// array of images, for random generation (if I can make some cats)
 const image = ["image", "image", "image", "image", "image", "image"];
+
+// <p> + button - basic setup of page
+
+//button click activate one array at a time
